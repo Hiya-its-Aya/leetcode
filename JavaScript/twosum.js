3276b511335f35bd0e3553719bce9bdcede83cb2
ayamoosa <ayamoosa98@gmail.com>
2023-04-01 23:50:51 -0700

    py twosum

--- JavaScript/twosum.js
+++ JavaScript/twosum.js
@@ -30,7 +30,7 @@ var twoSum = function(nums, target) {
   //take two pointers in two loops
   for (let i = 0; i < nums.length; i++){
       for (let j = i + 1; j < nums.length; j++){
-          if ( nums[i] + nums[j] === target) return [i, j]
+          if (nums[i] + nums[j] === target) return [i, j]
       }
   }
 };
